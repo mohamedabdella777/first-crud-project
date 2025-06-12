28cd583ddfdc467ea2a4b782f851243d398cec7a
mohamed <mohamedabdella192@gmail.com>
2025-06-12 20:41:59 +0300

    test

--- app.js
+++ app.js
@@ -1,4 +1,1062 @@
-//first step get all inputs
+//get inputs
+let title =document.getElementById('title');
+let price =document.getElementById('price');
+let taxes =document.getElementById('taxes');
+let ads =document.getElementById('ads');
+let discount=document.getElementById('discount');
+let total =document.getElementById('total');
+let count =document.getElementById('count');
+let category=document.getElementById('category');
+let create =document.getElementById('submit');
+let tbody=document.getElementById('tbody');
+let search =document.getElementById('search');
+let searchTitle =document.getElementById('searchTitle');
+let searchCategory =document.getElementById('searchCategory');
+let cleandata=document.getElementById('clean');
+let showdata=document.getElementById('show');
+let productsarry=JSON.parse(localStorage.getItem("products")) || [];
+
+
+ 
+//getTotal()
+ function getTotal() {
+  if (price.value !== '') {
+    let result = (+price.value + +taxes.value + +ads.value) - +discount.value;
+    total.innerHTML = result;
+    total.style.background = "#040"; // أخضر
+  } else {
+    total.innerHTML = '';
+    total.style.background = "#a00d02"; // الأحمر الافتراضي
+  }
+
+}
+ 
+create.addEventListener('click', () => {
+ tbody.innerHTML = '';
+ let userinputs={
+    title:title.value,
+    price:+price.value,
+    taxes:+taxes.value,
+    ads:+ads.value,
+    discount:+discount.value,
+    total:+total.innerHTML,
+    count:+count.value,
+    category:category.value
+ };
+
+   function make(){
+    if(title.value !== '' && price.value !== '' && count.value !== '' && category.value !== ''){
+      for (let i = 0; i < count.value; i++) {
+     
+    let tr=document.createElement('tr');
+    let td1=document.createElement('td');
+    let td2=document.createElement('td');
+    let td3=document.createElement('td');
+    let td4=document.createElement('td');
+    let td5=document.createElement('td');
+    let td6=document.createElement('td');
+    let td7=document.createElement('td');
+    let td8=document.createElement('td');
+    let td9=document.createElement('td');
+    let td10=document.createElement('td');
+    
+    
+    td1.innerHTML=i+1;
+    td2.innerHTML=title.value;
+    td3.innerHTML=price.value;
+    td4.innerHTML=taxes.value;
+    td5.innerHTML=ads.value;
+    td6.innerHTML=discount.value;
+    td7.innerHTML=total.innerHTML;
+    td8.innerHTML=category.value;
+    td9.innerHTML= `<button type="button"class="update" data-index="${i}">update</button>`;
+    td10.innerHTML=`<button type="button"class="delete" data-index="${i}">delete</button>`;
+  
+
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    tr.appendChild(td4);
+    tr.appendChild(td5);
+    tr.appendChild(td6);
+    tr.appendChild(td7);
+    tr.appendChild(td8);
+    tr.appendChild(td9);
+    tr.appendChild(td10);
+    tbody.appendChild(tr);  
+      }
+  }
+
+for(let i = 0; i < count.value; i++) {
+    productsarry.push(userinputs);
+   }
+  
+ 
+  
+  localStorage.setItem("products", JSON.stringify(productsarry));
+
+    title.value = '';
+    price.value = '';
+    taxes.value = '';
+    ads.value = '';
+    discount.value = '';
+    total.innerHTML = '';
+    count.value = '';
+    category.value = '';
+    total.style.background='#a00d02'; 
+
+    
+    
+  }
+  
+  
+   make();
+});
+
+
+  let searchMode;
+searchTitle.onclick= () =>{
+  if( search!==''){
+    search.value='';
+  }
+    searchMode='title';
+    search.placeholder="search by title";
+}
+searchCategory.onclick= () => {
+    if( search!==''){
+    search.value='';
+  }
+    searchMode='category';
+    search.placeholder="search by category";
+}
+
+ search.onkeyup= () => {
+    //let searchvalue = search.value.toLowerCase();
+    tbody.innerHTML = '';
+    function makesearch(){
+        
+  
+  for(let i=0;i<productsarry.length;i++){
+    if( productsarry[i].title === search.value.toLowerCase() && searchMode==='title'|| productsarry[i].category === search.value.toLowerCase() && searchMode==='category'){
+        
+       let tr =document.createElement('tr');
+       tr.innerHTML=
+       `<td>${i+1}</td>
+       <td>${productsarry[i].title}</td>
+       <td>${productsarry[i].price}</td>
+       <td>${productsarry[i].taxes}</td>
+       <td>${productsarry[i].ads}</td>
+       <td>${productsarry[i].discount}</td>
+       <td>${productsarry[i].total}</td>
+       <td>${productsarry[i].category}</td>
+       <td><button type="button"class="update" data-index="${i}">update</button></td>
+       <td><button type="button"class="delete" data-index="${i}">delete</button></td>`;
+       tbody.appendChild(tr);
+        
+    }
+      
+  }
+   
+ }
+    makesearch();
+   }
+
+
+    window.onload= () =>{
+   
+    tbody.innerHTML = '';
+  
+    productsarry.forEach((userinputs,i)  => {
+  
+    let tr=document.createElement('tr');
+    let td1=document.createElement('td');
+    let td2=document.createElement('td');
+    let td3=document.createElement('td');
+    let td4=document.createElement('td');
+    let td5=document.createElement('td');
+    let td6=document.createElement('td');
+    let td7=document.createElement('td');
+    let td8=document.createElement('td');
+    let td9=document.createElement('td');
+    let td10=document.createElement('td');
+    
+    
+    td1.innerHTML=i+1;
+    td2.innerHTML=userinputs.title;
+    td3.innerHTML=userinputs.price;
+    td4.innerHTML=userinputs.taxes;
+    td5.innerHTML=userinputs.ads;
+    td6.innerHTML=userinputs.discount;
+    td7.innerHTML=userinputs.total;
+    td8.innerHTML=userinputs.category;
+    td9.innerHTML= `<button type="button"class="update" data-index="${i}">update</button>`;
+    td10.innerHTML=`<button type="button"class="delete" data-index="${i}">delete</button>`;
+  
+
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    tr.appendChild(td4);
+    tr.appendChild(td5);
+    tr.appendChild(td6);
+    tr.appendChild(td7);
+    tr.appendChild(td8);
+    tr.appendChild(td9);
+    tr.appendChild(td10);
+    tbody.appendChild(tr);  
+
+ 
+});
+   cleandata.style.display='block';
+   cleandata.onclick= () => {
+    localStorage.clear();
+    tbody.innerHTML = '';
+    cleandata.style.display='none';
+   
+   }
+
+  }
+  
+  
+
+  tbody.onclick= (e) => {
+    if(e.target.classList.contains('delete')){
+        let index=e.target.dataset.index;//امسك ال الرقم بتاع العنصر اللي بيساوي نفس رقم زر الحذف وادخل علي المصفوفه واحذف منها عنصر واحد عند نفس الindex
+        productsarry.splice(index,1);
+        localStorage.setItem("products", JSON.stringify(productsarry));
+        
+    }
+     if(e.target.classList.contains('update')){
+          let index=e.target.dataset.index;
+
+          title.value=productsarry[index].title;
+          price.value=productsarry[index].price;
+          taxes.value=productsarry[index].taxes;
+          ads.value=productsarry[index].ads;
+          discount.value=productsarry[index].discount;
+          total.innerHTML=productsarry[index].total;
+          count.style.display = 'none';
+          category.value=productsarry[index].category;
+          getTotal();
+          create.innerHTML='update';
+           
+
+          
+          create.onclick= () => {
+            productsarry[index]={
+                title:title.value,
+                price:+price.value,
+                taxes:+taxes.value,
+                ads:+ads.value,
+                discount:+discount.value,
+                total:+total.innerHTML,
+                count:+count.value,
+                category:category.value
+            };
+        
+            localStorage.setItem("products", JSON.stringify(productsarry));
+      
+            create.innerHTML='create';
+            count.style.display = 'block';
+           
+
+        
+    
+            }
+          }
+ 
+  
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*//first step get all inputs
 let title = document.getElementById("title");
 let price = document.getElementById("price");
 let taxes = document.getElementById("taxes");
@@ -216,8 +1274,7 @@ function searchData(value){
     document.getElementById('tbody').innerHTML = table;
         }
 
-
-
+*/
 
 
   
